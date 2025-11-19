632ce581f936d61dec487fe031f77b10bddaf3a4
quicksxope <145250242+quicksxope@users.noreply.github.com>
2025-11-19 11:12:54 +0700

    Update dashboardapp.py

--- dashboardapp.py
+++ dashboardapp.py
@@ -11,6 +11,18 @@ load_dotenv()  # Baca .env
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Koneksi ke NeonDB
+conn = psycopg2.connect(DATABASE_URL)
+cur = conn.cursor()
+
+# Test query
+cur.execute("SELECT NOW();")
+print("Database waktu sekarang:", cur.fetchone())
+
+cur.close()
+conn.close()
+
+
 # ========== PAGE CONFIG ==========
 st.set_page_config(page_title="Sistem Logistik Tambak Udang", layout="wide")
 
